d82b0de04f4b5e71d281a5764ff2f99f84e48a21
sairam <133522255+sairammaddela@users.noreply.github.com>
2024-06-21 00:23:53 +0530

    index_1

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [response,setresponse]=useState("");
+  function submitHandler(e){
+    e.preventDefault();
+    // console.log(e.currentTarget[0].value);
+    
+      fetch(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${e.currentTarget[0].value}&lon=${e.currentTarget[1].value}`).then(data=>{
+        data.json().then(res=>{
+          console.log(res);
+          setresponse(res);
+        })
+      })
+    
+  }
   return (
     <>
-      <div id="root"></div>
+      <div id="root">
+        <h1>Weather Forecast</h1>
+        <form onSubmit={submitHandler}> 
+        <label>Latitude<input className="latitude"/></label>
+        <label>Latitude<input className="latitude"/></label>
+        <button type="submit">Get Forecast</button>
+        </form>
+        <table>
+          <thead>
+            <tr>
+              <th>Time</th>
+              <th>Temperature (°C)</th>
+              <th>Summary</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              response?.properties?.timeseries?.map(val=>{
+                return <tr><td>{val.time.toLocaleString()}</td><td>{val.data.instant.details.air_temperature}</td><td>{val.data.next_1_hours?.summary.symbol_code}</td></tr>
+              })
+            }
+          </tbody>
+        </table>
+        
+      </div>
     </>
   );
 }
